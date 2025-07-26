403ff38fed651bd25a3ef23fed196ba8055d47f0
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-07-26 14:25:35 +0100

    Update wander node

--- src/nodes/wander.clj
+++ src/nodes/wander.clj
@@ -3,14 +3,14 @@
             [mini-ros.motor-arbiter :refer [motor-control-locked?]]
             [mini-ros.core :refer [publish!]]))
 
-; (def directions [:forward :left :right])
-(def directions [:forward])
+(def directions [:forward :left :right])
 
 (defn start-wander-node []
   (go-loop []
     (when (not (motor-control-locked?)) ;; Only drive if motors are free
       (let [dir (rand-nth directions)]
+        (println "[wander node] wandering" dir)
         (publish! :wander/cmd {:dir dir :left-motor-speed 1.0 :right-motor-speed 1.0}))
-      (<! (timeout 100))) ; move a bit before next change
+      (<! (timeout 1000))) ; move a bit before next change
     (recur)))
 
