0b6f511708008ecbfbc1fff0d20d311258d9899a
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-07-26 14:20:20 +0100

    Fix avoidance

--- src/nodes/avoidance.clj
+++ src/nodes/avoidance.clj
@@ -12,31 +12,31 @@
   (go-loop []
     (let [distance (ultrasound/measure sensor)]
       (when (and (not @avoiding?) (>= distance 0.0) (<= distance threshold))
-          (do
-            (println "🛑 Obstacle detected!" distance "running avoidance pattern")
-            (lock-motor-control! :avoidance/cmd)
+        (do
+          (println "🛑 Obstacle detected!" distance "running avoidance pattern")
+          (lock-motor-control! :avoidance/cmd)
 
-            (reset! avoiding? true)
-            (publish! :brain/event :obstacle-detected)
+          (reset! avoiding? true)
+          (publish! :brain/event :obstacle-detected)
 
             ;; Reverse slowly
-            (println "* Reversing")
-            (publish! :avoidance/cmd {:dir :backward :left-motor-speed 0.8 :right-motor-speed 0.8})
-            (<! (timeout 500))
+          (println "* Reversing")
+          (publish! :avoidance/cmd {:dir :backward :left-motor-speed 0.8 :right-motor-speed 0.8})
+          (<! (timeout 500))
 
             ;; Turn left or right
-            (println "* Turning")
-            (publish! :avoidance/cmd {:dir (rand-nth [:left :right]) :left-motor-speed 0.8 :right-motor-speed 0.8})
-            (<! (timeout 750))
+          (println "* Turning")
+          (publish! :avoidance/cmd {:dir (rand-nth [:left :right]) :left-motor-speed 0.8 :right-motor-speed 0.8})
+          (<! (timeout 750))
 
             ;; Stop
-            (println "* Stopping")
-            (publish! :avoidance/cmd :stop)
-            (<! (timeout 100))
+          (println "* Stopping")
+          (publish! :avoidance/cmd :stop)
+          (<! (timeout 100))
 
-            (println "🛑 Obstacle cleared. Avoidance routine complete, handing control back to brain.")
+          (println "🛑 Obstacle cleared. Avoidance routine complete, handing control back to brain.")
             ;; Allow brain to take back control
-            (release-motor-control!)
-            (reset! avoiding? false)
-            (publish! :brain/event :obstacle-cleared)))))
+          (release-motor-control!)
+          (reset! avoiding? false)
+          (publish! :brain/event :obstacle-cleared))))
     (recur)))
