88a61d8c8556f5f65843a84598b279e1a8b8ae02
Paul Cockrell <paulcockrell@gmail.com>
2025-09-05 15:32:58 +0100

    Update where port is set.

--- src/web/backend/main.clj
+++ src/web/backend/main.clj
@@ -1,12 +1,10 @@
 (ns web.backend.main
   (:require [web.backend.router :as router]
-            [web.backend.env :as env]
             [web.backend.server :as server]))
 
-(defn start! []
-  (let [port (if env/is-rpi? 80 3000)]
-    (router/start!)
-    (server/start! port)))
+(defn start! [port]
+  (router/start!)
+  (server/start! port))
 
 (defn stop! []
   (router/stop!)
