23e57b8375499772299d7082f1526095310c918d
Paul Cockrell <paulcockrell@gmail.com>
2025-09-06 18:14:42 +0100

    Remove debug logging

--- src/robot/nodes/line_follow.clj
+++ src/robot/nodes/line_follow.clj
@@ -18,7 +18,6 @@
       (let [status (ldr/status ldr-sensor)]
         (reset! sensor-state status)
 
-        (println "XXX mode=" (get-mode) ", sub-mode=" (get-sub-mode))
         (when (and (not (motor-control-locked?)) (active-mode? :sentient :line-follow)) ;; Only drive if motors are free
           (let [{:keys [left middle right]} status
                 cmd (cond
