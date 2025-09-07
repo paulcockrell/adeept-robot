262b0ebbe39bcf52b77316446baf65e6a7aa8592
Paul Cockrell <paulcockrell@gmail.com>
2025-09-07 19:48:12 +0100

    Translate web issued commands to commands the motor arbiter can use

--- src/robot/nodes/web.clj
+++ src/robot/nodes/web.clj
@@ -9,9 +9,15 @@
   (let [ch (subscribe :robot/action)] ;TODO monitor all web-based topics: [:robot/action :camera/action :led/action]
     (go-loop []
       (let [{:keys [payload]} (<! ch)
-            {:keys [operating-mode]} @robot-state]
+            {:keys [operating-mode]} @robot-state
+            topic :manual/cmd]
         (when (= operating-mode :manual)
           (println "Received a manual instruction while in manual mode. Payload" payload)
-          (publish! :manual/cmd payload))) ; TODO we should filter this data so that only the motor arbiter gets motor commands
+          (case payload
+            :forward (publish! topic {:dir :forward :left-motor-speed 1.0 :right-motor-speed 1.0})
+            :backward (publish! topic {:dir :forward :left-motor-speed 0.8 :right-motor-speed 0.8})
+            :left (publish! topic {:dir :left})
+            :right (publish! topic {:dir :right})
+            :stop (publish! topic :stop)))) ; TODO we should filter this data so that only the motor arbiter gets motor commands
       (recur))))
 
