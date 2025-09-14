30bade815ddf8e6713c8abcb9d4108e16c3ea0f6
Paul Cockrell <paulcockrell@gmail.com>
2025-09-14 13:52:06 +0100

    Extend the mock camera to take the test image and embed timestamp before copying to destination

--- src/robot/hardware/mock/camera.clj
+++ src/robot/hardware/mock/camera.clj
@@ -1,5 +1,19 @@
 (ns robot.hardware.mock.camera
-  (:require [clojure.java.io :refer [copy resource input-stream file]]))
+  (:require [clojure.java.io :as io])
+  (:import [javax.imageio ImageIO]
+           [java.awt Color Font RenderingHints]))
 
-(defn capture-frame! [outfile]
-  (copy (input-stream (resource "test_frame.jpg")) (file outfile)))
+(defn capture-frame!
+  "Copies test_frame.jpg from resources and embeds frame
+  number before copying to /tmp/frame.jpg"
+  [outfile]
+  (let [src (ImageIO/read (io/resource "test_frame.jpg"))
+        graphics (.createGraphics src)]
+    (try
+      (.setFont graphics (Font. "SansSerif" Font/BOLD 28))
+      (.setColor graphics Color/RED)
+      (.setRenderingHint graphics RenderingHints/KEY_TEXT_ANTIALIASING
+                         RenderingHints/VALUE_TEXT_ANTIALIAS_ON)
+      (.drawString graphics (str (java.time.Instant/now)) 15 30)
+      (ImageIO/write src "jpg" (io/file outfile))
+      (finally (.dispose graphics)))))
