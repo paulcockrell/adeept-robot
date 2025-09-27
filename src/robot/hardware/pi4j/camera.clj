e7cfcfc24c728176240d336d848c88987c2e03ca
Paul Cockrell <paulcockrell@gmail.com>
2025-09-27 13:52:25 +0100

    Try to make camera work

--- src/robot/hardware/pi4j/camera.clj
+++ src/robot/hardware/pi4j/camera.clj
@@ -3,40 +3,42 @@
             [robot.mini-ros.core :refer [publish!]]
             [robot.mini-ros.state :refer [shutting-down?]])
   (:import [org.bytedeco.opencv.global opencv_imgcodecs]
+           [org.bytedeco.opencv.global opencv_videoio]
            [org.bytedeco.opencv.opencv_videoio VideoCapture]
            [org.bytedeco.opencv.opencv_core Mat]))
 
 (defonce ready (atom false))
 (defonce video-cap-instance (atom nil))
-(defonce frame-instance (atom nil))
 
 (defn capture-frame! []
   (reset! ready true))
 
 (defn create-camera [outfile]
-  (let [video-cap (VideoCapture. 0) ; 0 = default camera
-        frame (Mat.)]
-    (when-not (.isOpened video-cap)
+  (let [video-cap (VideoCapture. 0)] ; 0 = default camera
+    (if (.isOpened video-cap)
+      (do 
+        (.set video-cap opencv_videoio/CAP_PROP_FRAME_WIDTH 640)
+        (.set video-cap opencv_videoio/CAP_PROP_FRAME_HEIGHT 480))
       (throw (ex-info "[CAMERA] Camera could not be opened" {})))
+
+    (reset! video-cap-instance video-cap)
 
     (println "[CAMERA] Created")
 
-      ;; save references so they persist
-    (reset! video-cap-instance video-cap)
-    (reset! frame-instance frame)
-
     (go-loop []
-      (when (and @ready (not @shutting-down?))
-        (if (.read video-cap frame)
-          (do
-            (opencv_imgcodecs/imwrite outfile frame)
-            (reset! ready false))
-          (println "[CAMERA] Failed to capture frame")))
-      (recur))))
+      (when-not @shutting-down?
+        (if @ready
+          (let [frame (Mat.)]
+            (if (.read @video-cap-instance frame)
+              (do
+                (opencv_imgcodecs/imwrite outfile frame)
+                (reset! ready false))
+              (println "[CAMERA] Failed to capture frame")))
+          (<! (timeout 50))) ; wait 50ms when idle
+        (recur)))))
 
 (defn shutdown-camera! []
   (when @video-cap-instance
     (.release @video-cap-instance)
     (reset! video-cap-instance nil)
-    (reset! frame-instance nil)
     (println "[CAMERA] Shutdown")))
