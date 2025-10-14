e327b802e3cd3496e5be0a141211386f1d323ac7
Paul Cockrell <paulcockrell@gmail.com>
2025-10-14 19:41:44 +0100

    Try setting up raspberry pi driver different way

--- src/robot/hardware/pi4j/camera.clj
+++ src/robot/hardware/pi4j/camera.clj
@@ -1,24 +1,39 @@
 (ns robot.hardware.pi4j.camera
   (:require [clojure.core.async :refer [go-loop <! timeout]]
             [clojure.java.io :as io]
-            [robot.mini-ros.core :refer [publish!]]
             [robot.mini-ros.state :refer [shutting-down?]])
-  (:import [boofcv.io.webcamcapture UtilWebcamCapture]
-           [com.github.sarxos.webcam Webcam]
+  (:import [com.github.sarxos.webcam Webcam]
+           [com.github.sarxos.webcam.ds.raspivid RaspividDriver]
+           [java.awt Dimension]
            [javax.imageio ImageIO]))
 
 (defonce ready (atom false))
 (defonce webcam-instance (atom nil))
 
+(defn- maybe-set-raspi-driver! []
+  (try
+    (Webcam/setDriver (RaspividDriver.))
+    (println "[CAMERA] Using Raspivid driver")
+    (catch Throwable _
+      (println "[CAMERA] Raspivid driver unavailable, falling back to default"))))
+
+(defn- configure-webcam! [^Webcam webcam width height]
+  (let [size (Dimension. width height)]
+    (.setCustomViewSizes webcam (into-array Dimension [size]))
+    (.setViewSize webcam size)))
+
 (defn capture-frame! []
   (reset! ready true))
 
 (defn create-camera [outfile]
-  (let [webcam (UtilWebcamCapture/openDefault 640 480)]
+  (maybe-set-raspi-driver!)
+  (let [webcam (Webcam/getDefault)]
     (when (nil? webcam)
       (throw (ex-info "[CAMERA] Camera could not be opened" {})))
+
+    (configure-webcam! webcam 640 480)
     (when-not (.isOpen webcam)
-      (.open webcam))
+      (.open webcam true))
 
     (reset! webcam-instance webcam)
     (println "[CAMERA] Created")
@@ -27,7 +42,7 @@
       (when-not @shutting-down?
         (if @ready
           (try
-            (let [frame (UtilWebcamCapture/getImage ^Webcam @webcam-instance)]
+            (let [frame (.getImage ^Webcam @webcam-instance)]
               (if frame
                 (do
                   (ImageIO/write frame "jpg" (io/file outfile))
