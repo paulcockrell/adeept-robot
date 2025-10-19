aaeb3235be41069a9bca37c4451ea33fbacd3a96
Paul Cockrell <paulcockrell@gmail.com>
2025-10-19 14:53:31 +0100

    Remove unwanted arg

--- src/robot/hardware/camera.clj
+++ src/robot/hardware/camera.clj
@@ -5,7 +5,7 @@
 
 (defn create-camera [outfile]
   (if env/is-rpi?
-    (camera/create-camera "http://127.0.0.1:8081/stream.mjpg") ;; TODO - move this to an argument
+    (camera/create-camera)
     (mock-camera/create-camera outfile)))
 
 (defn shutdown-camera! []
